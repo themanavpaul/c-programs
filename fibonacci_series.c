67d548a51e4168f836f005e1e20f77bd49b8cef0
Manav Paul <39908876+themanavpaul@users.noreply.github.com>
2022-12-11 08:55:23 +0530

    Create fibonacci_series.c

---
+++ fibonacci_series.c
@@ -0,0 +1,21 @@
+// Fibonacci Series of nth term
+#include <stdio.h>
+int main() {
+  int i, n;
+  int t1 = 0, t2 = 1;  // initialize first and second terms
+  int nextTerm = t1 + t2;   // initialize the next term (3rd term)
+  printf("Enter the number of terms: ");
+  scanf("%d", &n);
+
+  // print the first two terms t1 and t2
+  printf("Fibonacci Series: %d, %d, ", t1, t2);
+
+  // print 3rd to nth terms
+  for (i = 3; i <= n; ++i) {
+    printf("%d, ", nextTerm);
+    t1 = t2;
+    t2 = nextTerm;
+    nextTerm = t1 + t2;
+  }
+  return 0;
+}
