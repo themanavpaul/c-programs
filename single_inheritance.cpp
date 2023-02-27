5c67ab61f7226a170b3e777cf287192f26404986
Manav Paul <39908876+themanavpaul@users.noreply.github.com>
2023-02-27 15:12:10 +0530

    Create single_inheritance.cpp

---
+++ single_inheritance.cpp
@@ -0,0 +1,39 @@
+//Single level Inheritance in C++ 
+// Program to print rectangle area through single inheritance
+
+#include <iostream>
+using namespace std;
+
+// Base class
+class Shape {
+   public:
+      void setWidth(int w) {
+         width = w;
+      }
+      void setHeight(int h) {
+         height = h;
+      }
+   protected:
+      int width;
+      int height;
+};
+
+// Derived class
+class Rectangle: public Shape {
+   public:
+      int getArea() {
+         return (width * height);
+      }
+};
+
+int main() {
+   Rectangle Rect;
+
+   Rect.setWidth(5);
+   Rect.setHeight(7);
+
+   // Print the area of the object.
+   cout << "Total area: " << Rect.getArea() << endl;
+
+   return 0;
+}
