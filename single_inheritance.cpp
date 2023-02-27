ae09f90156121608af19ad33c387bf14db209209
Manav Paul <39908876+themanavpaul@users.noreply.github.com>
2023-02-27 15:12:50 +0530

    Update Single_inheritance.cpp

--- single_inheritance.cpp
+++ single_inheritance.cpp
@@ -1,39 +1,39 @@
 //Single level Inheritance in C++ 
 // Program to print rectangle area through single inheritance
 
-#include <iostream>
-using namespace std;
+#include <iostream>  // include the standard input/output library
+using namespace std; // use the standard namespace
 
 // Base class
-class Shape {
-   public:
-      void setWidth(int w) {
+class Shape {   // declare a class called Shape
+   public:      // public members of the class
+      void setWidth(int w) {  // declare a function to set the width of the shape
          width = w;
       }
-      void setHeight(int h) {
+      void setHeight(int h) {  // declare a function to set the height of the shape
          height = h;
       }
-   protected:
-      int width;
-      int height;
+   protected:   // protected members of the class
+      int width;   // declare a protected variable for the width of the shape
+      int height;  // declare a protected variable for the height of the shape
 };
 
 // Derived class
-class Rectangle: public Shape {
-   public:
-      int getArea() {
+class Rectangle: public Shape {  // declare a class called Rectangle that inherits from the Shape class
+   public:      // public members of the class
+      int getArea() {  // declare a function to get the area of the rectangle
          return (width * height);
       }
 };
 
-int main() {
-   Rectangle Rect;
+int main() {   // main function of the program
+   Rectangle Rect;  // create an object of the Rectangle class called Rect
 
-   Rect.setWidth(5);
-   Rect.setHeight(7);
+   Rect.setWidth(5);    // set the width of the rectangle to 5
+   Rect.setHeight(7);   // set the height of the rectangle to 7
 
    // Print the area of the object.
-   cout << "Total area: " << Rect.getArea() << endl;
+   cout << "Total area: " << Rect.getArea() << endl;  // print the area of the rectangle
 
-   return 0;
+   return 0;    // exit the program
 }
